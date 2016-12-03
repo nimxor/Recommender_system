995d026324b051e0a1e195d2c5cf62d3b97c8287
Your Name <shubhamgupta190496@gmail.com>
2016-12-03 18:43:33 +0530

    initial commit

---
+++ Recommender_system.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi                    first
+#define se                    second
+#define ll                    long long
+#define all(a)                a.begin(),a.end()
+#define rall(a)               a.rbegin(),a.rend()  
+#define mod                  1000000007
+#define mp                    make_pair
+#define pb                    push_back
+#define mset(m,v)             memset(m,v,sizeof(m))
+
+const int MAXM = 1e5+3;
+ll a[MAXM];
+map<ll,ll> m;
+
+int main()
+{
+	
+	return 0;
+}
